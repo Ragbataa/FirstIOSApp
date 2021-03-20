8eced1d69378cb22e094e342c4f24ff45c13380d
Ragbataa <80989666+Ragbataa@users.noreply.github.com>
2021-03-19 22:34:29 -0400

    Initial Commit

---
+++ Shared/FirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppApp.swift
+//  Shared
+//
+//  Created by Ragbat Ali Ansari on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
